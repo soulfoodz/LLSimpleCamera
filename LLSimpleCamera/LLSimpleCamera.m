5407c6c47aa0ef2b12e8284f07088cad5fba4872
Eric Webb <ericwebb85@yahoo.com>
2016-05-06 13:36:09 -0500

    Fix for dark images

--- LLSimpleCamera/LLSimpleCamera.m
+++ LLSimpleCamera/LLSimpleCamera.m
@@ -304,6 +304,9 @@ NSString *const LLSimpleCameraErrorDomain = @"LLSimpleCameraErrorDomain";
              onCapture(self, image, metadata, error);
          }
      }];
+ 
+ // freeze the screen AFTER the capture call to eliminate the occasional dark image
+  [self.captureVideoPreviewLayer.connection setEnabled:NO];
 }
 
 -(void)capture:(void (^)(LLSimpleCamera *camera, UIImage *image, NSDictionary *metadata, NSError *error))onCapture {
